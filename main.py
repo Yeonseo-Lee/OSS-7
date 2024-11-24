e7ed0aa0a7b4b59609956f9607e34531291d9d38
Yeonseo Lee <dustj0406@naver.com>
2024-11-24 23:17:06 +0900

    Revert "Add files via upload"
    This reverts commit 6509182b996553e6b5a3911ac2e0c157197a4c18.

--- main.py
+++ main.py
@@ -19,16 +19,6 @@ ev3 = EV3Brick()
 
 
 # Write your program here.
-from pybricks.robotics import DriveBase
-
-ev3 = EV3Brick()
-
-ev3.speaker.beep()
-
-left_motor = Motor(Port.A)
-right_motor = Motor(Port.D)
-grab_motor= Motor(Port.B)
-shooting_motor = Motor(Port.C)
 left_motor = Motor(Port.C)
 right_motor = Motor(Port.D)
 
@@ -54,6 +44,37 @@ robot.turn(90)
 shoot.run(2000)
 time.sleep(0.25)
 shoot.stop()
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+from pybricks.robotics import DriveBase
+
+ev3 = EV3Brick()
+
+
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+grab_motor= Motor(Port.B)
+
+wheel_diameter = 5.6
+axle_track = 115
+
 robot = DriveBase(left_motor,right_motor,wheel_diameter,axle_track) 
 
 grab_motor.run_until_stalled(-200,then = Stop.COAST,duty_limit = 50)
@@ -96,25 +117,3 @@ grab_motor.run_until_stalled(-200,stop.COAST,duty_limit = 50)
 shooting_motor.run(2000)
 time.sleep(0.25)
 shooting_motor.stop()
-<<<<<<< HEAD
-robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
-sensor1 = GyroSensor(Port.S4)
-grab = Motor(Port.A)
-shoot = Motor(Port.B)
-
-grab.run_until_stalled(-100, then = Stop.COAST, duty_limit = 50)
-grab.reset_angle(0)
-grab.run_target(-100, 100)
-robot.straight(100)
-grab.run_until_stalled(100, then = Stop.COAST, duty_limit = 50)
-grab.reset_angle(0)
-grab.run_until_stalled(-100, then = Stop.COAST, duty_limit = 50)
-
-robot.straight(-50)
-robot.turn(90)
-
-shoot.run(2000)
-time.sleep(0.25)
-shoot.stop()
-=======
->>>>>>> LEE
