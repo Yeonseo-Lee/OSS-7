5b365873cb69259e0b0abcbd3499af1a1b9c406b
Yeonseo-Lee <dustj0406@naver.com>
2024-11-24 17:43:52 +0900

    Upload ball shooting

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+import time
+
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
@@ -17,4 +19,28 @@ ev3 = EV3Brick()
 
 
 # Write your program here.
-ev3.speaker.beep()
+left_motor = Motor(Port.C)
+right_motor = Motor(Port.D)
+
+wheel_diameter = 5.6
+axle_track = 115
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+sensor1 = GyroSensor(Port.S4)
+grab = Motor(Port.A)
+shoot = Motor(Port.B)
+
+grab.run_until_stalled(-100, then = Stop.COAST, duty_limit = 50)
+grab.reset_angle(0)
+grab.run_target(-100, 100)
+robot.straight(100)
+grab.run_until_stalled(100, then = Stop.COAST, duty_limit = 50)
+grab.reset_angle(0)
+grab.run_until_stalled(-100, then = Stop.COAST, duty_limit = 50)
+
+robot.straight(-50)
+robot.turn(90)
+
+shoot.run(2000)
+time.sleep(0.25)
+shoot.stop()
